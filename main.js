bd800ebb30249e5c1b76e06759278fa38b305414
Stephen P <ruzzo@umich.edu>
2023-01-30 23:51:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* New main file for the Beer Menu Scanner. From here you can upload an image and view the Scanner results */
+/* New main file for the Beer Menu Scanner. From here you can upload an image and view the Scanner results. */
 import { Heading } from '@aws-amplify/ui-react';
 
 <div style={{padding:"16px"}} id="about">
