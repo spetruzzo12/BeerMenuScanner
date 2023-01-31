cb85ede9485a10e5c5ce5fc39c11421648fb4463
Stephen P <ruzzo@umich.edu>
2023-01-30 23:58:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 /* New main file for the Beer Menu Scanner. From here you can upload an image and view the Scanner results. */
+
+import { Amplify } from 'aws-amplify';
+import awsExports from '../aws-exports';
 import { Heading } from '@aws-amplify/ui-react';
 
-<div style={{padding:"16px"}} id="about">
+Amplify.configure(awsExports);
+
+const Main = () => {
+    return (
+        <div style={{padding:"16px"}} id="about">
         <Heading style = {{textAlign: "center"}} level={1}>BEER MENUE SCANNER</Heading>
-</div>
+        </div> 
+    )
+}
+
+export default Main
