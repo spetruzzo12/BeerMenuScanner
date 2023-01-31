6da219feaf3f93500eae7161cf15c36141dc7959
Stephen P <ruzzo@umich.edu>
2023-01-31 01:51:48 -0500

    Update App.js
    Added file upload fields and buttons. Not working with Lambda or S3 yet.

--- BeerMenuScannerApp/src/App.js
+++ BeerMenuScannerApp/src/App.js
@@ -1,11 +1,32 @@
-import { Heading } from '@aws-amplify/ui-react';
+import React from 'react';
+import { Heading, Button } from '@aws-amplify/ui-react';
 
-function App() {
-  return (
-    <div style={{padding:"16px"}}>
-    <Heading style = {{textAlign: "center"}} level={1}>BEER MENU SCANNER</Heading>
-    </div> 
-  );
+class App extends React.Component 
+{
+  state = {selectedImage: null}
+
+  fileSelectedHandler = event =>
+  {
+    this.state.selectedImage = event.target.files[0];
+  }
+
+  uploadImage = () =>
+  {
+    const fd = new FormData();
+    fd.append('image', this.state.selectedImage, this.state.selectedImage.name);
+    /* Send to Lambda function from here */
+  }
+
+  render() 
+  {
+    return (
+      <div style={{textAlign: "center"}}>
+        <Heading level={1}>BEER MENU SCANNER</Heading>
+        <input type="file" onChange={this.fileSelectedHandler}/>
+        <Button onClick={this.uploadImage}>Upload</Button>
+      </div> 
+    );
+  }
 }
 
 export default App;
