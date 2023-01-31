f6b13cf4be8f68c5446797d259318e781b04c08a
Stephen P <ruzzo@umich.edu>
2023-01-31 00:30:34 -0500

    Setting up react

---
+++ BeerMenuScannerApp/src/App.js
@@ -0,0 +1,11 @@
+import { Heading } from '@aws-amplify/ui-react';
+
+function App() {
+  return (
+    <div style={{padding:"16px"}}>
+    <Heading style = {{textAlign: "center"}} level={1}>BEER MENU SCANNER</Heading>
+    </div> 
+  );
+}
+
+export default App;
