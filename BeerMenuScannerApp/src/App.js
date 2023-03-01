b5580072657816eab7f8490ccb4eb2c4e7b978b1
Stephen P <ruzzo@umich.edu>
2023-03-01 00:41:24 -0500

    Update App.js

--- BeerMenuScannerApp/src/App.js
+++ BeerMenuScannerApp/src/App.js
@@ -3,7 +3,9 @@ import { Heading, Button } from '@aws-amplify/ui-react';
 
 class App extends React.Component 
 {
-  state = { selectedImage: null, base64: "" };
+  state = { selectedImage: null, base64: "", beerResults: "" };
+  infoDict = {"old_names": "", "correct_ratio": "", "idx": "", "Name": "", "Style": "", "ABV": "", "Ratings": "", "AVG": "", "Last Active": "", "brewery": "", "brewery_link": "", "beer_link": "", "beer_brewery": ""};
+  infoDictMap = ["old_names", "correct_ratio", "idx", "Name", "Style", "ABV", "Ratings", "Avg", "Last Active", "brewery", "brewery_link", "beer_link", "beer_brewery"];
 
   /* Get the selected image and properties */
   fileSelectedHandler = async (e) =>
@@ -38,6 +40,7 @@ class App extends React.Component
   };
 
   /* On "Upload" click, API Gateway -> Lambda -> S3 */
+  /* Using a fetch and prmoise to send data */
   uploadImage = () =>
   {
     /* Send to Lambda function from here */
@@ -59,7 +62,8 @@ class App extends React.Component
   }
 
   /* Get the Beer Results if they exist */
-  getBeerResults = () =>
+  /* Not using a fetch and prmoise as we want this to be async and store the date */
+  async getBeerResults()
   {
     /* Call API to check for results if they exist */
     var myHeaders = new Headers();
@@ -72,11 +76,45 @@ class App extends React.Component
       redirect: 'follow'
     };
     /* Execute the POST */
-    fetch("https://17yiu3g03f.execute-api.us-east-1.amazonaws.com/BeerMenuGet", requestOptions)
-    .then(response => response.text())
-    // .then(result => alert(JSON.parse(result).statusCode + " | " + JSON.parse(result).body))
-    .then(result => console.log(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+    const response = await fetch("https://17yiu3g03f.execute-api.us-east-1.amazonaws.com/BeerMenuGet", requestOptions)
+    const data = await response.json();
+    this.state.beerResults = String(data.body).replace(/["]/g, "");
+    // console.log(this.state.beerResults);
+
+    /* Parse the dirty dirty data...wish it was in JSON */
+    var beginIdx = 0; var endIdx = 0;
+    if (data.statusCode == 200)
+    {
+      for (let i = 0; i < this.infoDictMap.length - 1; i++)
+      {
+        // Get beginning of string index
+        beginIdx = this.state.beerResults.indexOf(this.infoDictMap[i]) + this.infoDictMap[i].length + 2;
+
+        // Get end of string index
+        if (i == this.infoDictMap.length - 1)
+          endIdx = this.state.beerResults.length - 2;
+        else
+          endIdx = this.state.beerResults.indexOf(this.infoDictMap[i+1]) - 2;
+
+        // Split the string into an array
+        this.infoDict[i] = this.state.beerResults.slice(beginIdx, endIdx).split(',');
+
+        // Delete the first two characters in each array
+        for (let j = 0; j < this.infoDict[i].length; j++)
+          this.infoDict[i][j] = this.infoDict[i][j].slice(2);
+
+        // Set input string to remaining string to be analyzed
+        this.state.beerResults = this.state.beerResults.slice(endIdx);
+      }
+    }
+    console.log(this.infoDict);
+  }
+
+  /* Handles the get beer results click, needed to have an async function */
+  handleBeerResultsClick = (e) =>
+  {
+    e.preventDefault();
+    this.getBeerResults();
   }
 
   render() 
@@ -88,7 +126,7 @@ class App extends React.Component
           <Heading level={1}>BEER MENU SCANNER</Heading>
           <input type="file" onChange={this.fileSelectedHandler}/>
           <Button onClick={this.uploadImage}>Upload</Button><br></br>
-          <Button onClick={this.getBeerResults}>Get Results!</Button>
+          <Button onClick={this.handleBeerResultsClick}>Get Results!</Button>
         </div> 
       </div>
     );
